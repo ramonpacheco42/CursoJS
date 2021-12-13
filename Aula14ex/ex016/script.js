d48f48d44ae7f3eb0186e2c0828acae142357595
ramonpacheco42 <74688664+ramonpacheco42@users.noreply.github.com>
2021-12-13 08:14:22 -0300

    javascript

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO!!] faltam dados')
+    } else {
+        res.innerHTML = 'Contando....'
+    }
+}
